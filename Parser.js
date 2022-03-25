04aa0e2a05ee99c9e7c8160ab0190ff6283efadb
Grey <greywed@icloud.com>
2022-03-24 17:59:53 -0700

    added parser functionality

---
+++ Parser.js
@@ -0,0 +1,44 @@
+class Parser {
+    constructor(script) {
+        this.script = script;
+    }
+
+    parse() {
+        let errorsfound = 0
+
+        console.log("Receaved program: ")
+        let current = ''
+        let allLines = this.script.split('\n')
+        console.log(allLines)
+        allLines.forEach(line => {
+            if (line.length > 1){
+                current = line.split(" ")
+                this.execute(current)
+            }
+        })
+
+        return errorsfound;
+    }
+    execute(c){
+        if(c[0] == 'log'){
+            c.shift();
+            if (c[0].includes("'")){
+                console.log(c.join(" "))
+            }
+        }else{
+            console.log("Undefined reference : " + c[0])
+        }
+    }
+}
+
+let script = `
+
+log 'Hello, World'
+add(6,3)
+x = add(6,4)
+log x
+
+`
+
+let parser =  new Parser(script);
+parser.parse()
