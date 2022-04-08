6c7186bfff475e5177b4ab5c99a2b834951939cf
Grey <greywed@icloud.com>
2022-04-07 17:56:51 -0700

    added moduals for fileIO

--- FileIO.js
+++ FileIO.js
@@ -1,10 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-let currentpath = "C:\\Users\\17027\\Documents\\visualstudio\\Javascript\\hello.txt"
-console.log(path.basename(currentpath))
-
-fs.readFile(currentpath, 'utf8',function (err, data){
+fs.readFile(__dirname + '\\hello.txt', 'utf8',function (err, data){
     if (err) { return console.log(err)}
     console.log(data)
 })
